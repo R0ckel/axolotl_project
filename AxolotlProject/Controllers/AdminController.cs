a95f50a1d88ed5a8a93bd91b387d5e47038375e3
RomanKoshovyi <romanrokel@gmail.com>
2022-11-19 11:30:34 +0200

    Admin panel beta version (binded, added to navigation)

---
+++ AxolotlProject/Controllers/AdminController.cs
@@ -0,0 +1,56 @@
+﻿using AxolotlProject.Data;
+using AxolotlProject.Models;
+using AxolotlProject.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.Intrinsics.X86;
+
+namespace AxolotlProject.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly ILogger<UserPostController> _logger;
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<User> _userManager;
+
+        public AdminController(ILogger<UserPostController> logger, ApplicationDbContext context, UserManager<User> userManager)
+        {
+            _logger = logger;
+            _context = context;
+            _userManager = userManager;
+        }
+        public IActionResult Index(string? login = null)
+        {
+            var vm = new AdminViewModel();
+            vm.searchedUsers = _context.Users
+                .Where(u=>u.Login==null || login==null || u.Login.Contains(login))
+                .Where(u=>u.IsBanned == false);
+            vm.bannedUsers = _context.Users.Where(u => u.IsBanned);
+            return View(vm);
+        }
+
+        [HttpPost]
+        public IActionResult Ban(string id)
+        {
+            var toBan = _context.Users.Where(u => u.Id == id).FirstOrDefault();
+            if (toBan != null)
+            {
+                toBan.IsBanned = true;
+            }
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult UnBan(string id)
+        {
+            var toBan = _context.Users.Where(u => u.Id == id).FirstOrDefault();
+            if (toBan != null)
+            {
+                toBan.IsBanned = false;
+            }
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ AxolotlProject/ViewModels/AdminViewModel.cs
@@ -0,0 +1,10 @@
+﻿using AxolotlProject.Models;
+
+namespace AxolotlProject.ViewModels
+{
+    public class AdminViewModel
+    {
+        public IEnumerable<User> searchedUsers { get; set; }
+        public IEnumerable<User> bannedUsers { get; set; }
+    }
+}
